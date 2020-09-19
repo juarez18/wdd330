eae85ac60d23f5dde9ed38ca8b986b1e21a94431
juarez18 <marcusjuarez18@gmail.com>
2020-09-19 00:27:32 -0600

    update

---
+++ index/js/main.js
@@ -0,0 +1,23 @@
+const requestURL = "/index/data/links.json";
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    //console.table(jsonObject); 
+    const links = jsonObject['links'];
+    for (let i = 0; i < links.length; i++ ) {
+        let li = document.createElement("li");
+        let a = document.createElement("a");
+
+        a.textContent = links[i].label;
+        a.setAttribute("href", links[i].url);
+
+        li.appendChild(a);
+
+        document.getElementById('assignment-list').appendChild(li);
+
+    }
+    
+});
