a58af1311ef432e2578e8f75a2a9f01b642091c7
juarez18 <marcusjuarez18@gmail.com>
2020-12-16 14:57:48 -0700

    update

--- putting/js/putting.js
+++ putting/js/putting.js
@@ -6,12 +6,31 @@ const miss = document.getElementById("miss-putt");
 let stats = document.getElementById("putting-stats-display");
 let startRound = document.getElementById("start-round-menu");
 let puttingMenu = document.getElementById("active-putting");
-let endBtn = document.getElementById("end-round");
-let putts10 = [];
-let putts20 = [];
-let putts30 = [];
-let puttsc2 = [];
+const endBtn = document.getElementById("end-round");
+let current10 = document.getElementById("current10")
+let last10 = document.getElementById("last10")
+let lastRound = document.getElementById("lastround")
+const resetBtn = document.getElementById("reset-rounds");
+
+let putts10 = localStorage.getItem("10ft") ? JSON.parse(localStorage.getItem("10ft")) : []
+localStorage.setItem('10ft' , JSON.stringify(putts10))
+
+let putts20 = localStorage.getItem("20ft") ? JSON.parse(localStorage.getItem("20ft")) : []
+localStorage.setItem('20ft' , JSON.stringify(putts20))
+
+let putts30 = localStorage.getItem("30ft") ? JSON.parse(localStorage.getItem("30ft")) : []
+localStorage.setItem('30ft' , JSON.stringify(putts30))
+
+let puttsc2 = localStorage.getItem("c2") ? JSON.parse(localStorage.getItem("c2")) : []
+localStorage.setItem('c2' , JSON.stringify(puttsc2))
+
+
+
+
 let a = 0;
+let b = 0;
+let c = 0;
+
 let currentPutt = document.getElementById("current");
 let distance = document.getElementById("putting-distance").value;
 
@@ -20,6 +39,8 @@ function changeDisplay() {
     stats.classList.toggle("hidden")
     startRound.classList.toggle("hidden")
     puttingMenu.classList.toggle("hidden")
+    startRound.classList.toggle("start-round")
+    puttingMenu.classList.toggle("putting-session")
 }
 
 //event listener for start round
@@ -33,20 +54,39 @@ startBtn.onclick = function startround() {
 
 }
 
+resetBtn.onclick = function resetStorage() {
+    localStorage.clear();
+    putts10 = []
+    putts20 = []
+    putts30 = []
+    puttsc2 = []
+    lastRound.textContent = "Not Avaliable"
+    calcStats()
+    
+}
+
 //event listener for when you end the round early
 endBtn.onclick = function endround() {
+    lastRound.textContent = c + " out of " + a + " were made from " + distance + "!"
     changeDisplay()
     a = 0;
+    c = 0;
     calcStats()
+    current10.textContent = " "
+    last10.textContent = " "
 }
 
 //This checks your round to end it automatically
 function checkRound(current){
     let maxPutts = document.getElementById("putting-number").value
     if(current == maxPutts ){
+        lastRound.textContent = c + " out of " + a + " were made from " + distance + "!"
         changeDisplay()
+        c = 0;
         a = 0;
         calcStats()
+        current10.textContent = " "
+        last10.textContent = " "
     }
 
 }
@@ -55,42 +95,70 @@ function checkRound(current){
 make.onclick = function madeputt() {
     if (distance == "10ft") {
         putts10[putts10.length] = 1;
+        localStorage.setItem('10ft' , JSON.stringify(putts10));
+        c++;
         a++;
         currentPutt.textContent = a;
     } else if (distance == "20ft") {
         putts20[putts20.length] = 1;
+        localStorage.setItem('20ft' , JSON.stringify(putts20));
         a++;
+        c++;
         currentPutt.textContent = a;
     } else if (distance == "30ft") {
         putts30[putts30.length] = 1;
+        localStorage.setItem('30ft' , JSON.stringify(putts30));
         a++;
+        c++;
         currentPutt.textContent = a;
-    } else if (distance == "circle2") {
+    } else if (distance == "C2") {
         puttsc2[puttsc2.length] = 1;
+        localStorage.setItem('c2' , JSON.stringify(puttsc2));
         a++;
+        c++;
         currentPutt.textContent = a;
     }
+    if(a % 10 != 0){
+        b++;
+        current10.textContent = b + " / 10";
+    }
+    else if(a % 10 == 0){
+        b++
+        last10.textContent = b + " / 10";
+        b = 0;
+        current10.textContent = b + " / 10";
+    }
     checkRound(a)
+
 }
 
 //This records when you miss a putt
 miss.onclick = function missputt() {
     if (distance == "10ft") {
         putts10[putts10.length] = 0;
+        localStorage.setItem('10ft' , JSON.stringify(putts10));
         a++;
         currentPutt.textContent = a;
     } else if (distance == "20ft") {
         putts20[putts20.length] = 0;
+        localStorage.setItem('20ft' , JSON.stringify(putts20));
         a++;
         currentPutt.textContent = a;
     } else if (distance == "30ft") {
         putts30[putts30.length] = 0;
+        localStorage.setItem('20ft' , JSON.stringify(putts20));
         a++;
         currentPutt.textContent = a;
-    } else if (distance == "circle2") {
+    } else if (distance == "C2") {
         puttsc2[puttsc2.length] = 0;
+        localStorage.setItem('c2' , JSON.stringify(puttsc2));
         a++;
         currentPutt.textContent = a;
+    }
+    if(a % 10 == 0){
+        last10.textContent = b + " / 10";
+        b = 0;
+        current10.textContent = b + " / 10";
     }
     checkRound(a)
 }
@@ -140,3 +208,6 @@ function calcStats() {
     displayStats(totalC2Putts, makesc2putt, "c2-percentage");
     displayStats(totalPutts, totalmakes, "total-percentage");
 }
+
+calcStats()
+
